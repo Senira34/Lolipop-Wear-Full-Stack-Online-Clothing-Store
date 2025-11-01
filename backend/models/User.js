41dd382b1868262cde31a2af9c123eac229de398
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-11-01 20:56:38 +0530

    Create and UpdateUser.js

---
+++ backend/models/User.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  phone: {
+    type: String
+  },
+  address: [{
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String,
+    isDefault: {
+      type: Boolean,
+      default: false
+    }
+  }],
+  cart: [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product'
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    },
+    size: String,
+    color: String
+  }]
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+// Use an async middleware that returns a promise instead of calling next(),
+// and return early if the password wasn't modified.
+userSchema.pre('save', async function() {
+  // If password wasn't modified, do nothing
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

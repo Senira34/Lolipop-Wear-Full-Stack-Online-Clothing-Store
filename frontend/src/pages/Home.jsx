70a15fc814f919b3e1b449f2fbadcdfb0c92b58d
Senira Sachinthaka <Senura147sachinthaka@gmail.com>
2025-11-01 19:41:34 +0530

    Update Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import NewArrival from '../components/Newarrival'
+import Mens from '../components/Mens'
+import Women from '../components/Women'
+import Category from '../components/Category'
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <NewArrival />
+        <Mens />
+        <Women />
+        <Category />
+    </>
+  )
+}
+
+export default Home
